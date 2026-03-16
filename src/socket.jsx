4fe7dbd91f065b8e360bf069c48967d14af5e37f
zizuaine <Itsuki7444@gmail.com>
2026-03-16 19:12:24 +0530

    completed socket server and socket-client

---
+++ src/socket.jsx
@@ -0,0 +1,12 @@
+import { io } from 'socket.io-client';
+
+export const initSocket = async () => {
+    const options = {
+        forceNew: true,
+        reconnectionAttempts: Infinity,
+        timeout: 10000,
+        transports: ["websocket"],
+    };
+
+    return io(import.meta.env.VITE_BACKEND_URL, options)
+}
