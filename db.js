cc41dbfe2fbe42475eaa7650941f2170eebaea20
zizuaine <Itsuki7444@gmail.com>
2026-03-19 03:04:11 +0530

    mongo changes

--- db.js
+++ db.js
@@ -2,10 +2,17 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log(process.env.MONGO_URL);
-    } catch (err) {
-        console.log(err)
-    }
 
-}
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+
+        console.log("✅ MongoDB connected");
+        console.log("Host:", conn.connection.host);
+        console.log("DB:", conn.connection.name);
+
+    } catch (err) {
+
+        console.log("❌ Mongo error");
+        console.log(err);
+
+    }
+};
