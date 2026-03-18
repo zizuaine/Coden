fac770747fc2aec20f766de1ebe60ad4e777c2f9
zizuaine <Itsuki7444@gmail.com>
2026-03-18 16:01:48 +0530

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors())
 app.use(express.static('dist'));
 
-app.use("*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
