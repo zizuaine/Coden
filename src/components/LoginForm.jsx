3fe732ded4e1dfb3329f68afcba424e2b1e373dd
zizuaine <Itsuki7444@gmail.com>
2026-03-22 22:32:09 +0530

    finsihed auth

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
